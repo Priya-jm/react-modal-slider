fa8b91b01c2ad563605b2234770e477e923733b0
mallchel <sebastianalfabc@gmail.com>
2018-11-07 22:41:24 +0300

    add demo

---
+++ demo/precache-manifest.f92ce4e5ba94dda2e447c5a1b3f8c536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad9f05dbe0c3917d1d7",
+    "url": "/static/css/main.1cbf9ae0.chunk.css"
+  },
+  {
+    "revision": "5ad9f05dbe0c3917d1d7",
+    "url": "/static/js/main.5ad9f05d.chunk.js"
+  },
+  {
+    "revision": "9ecbf70e510f8a2720d8",
+    "url": "/static/js/1.9ecbf70e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efd9500ffd9f4821ef9376dea70c0d96",
+    "url": "/index.html"
+  }
+];
