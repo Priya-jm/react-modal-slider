a41fd79396790c8308a3b775cf3184984b795829
mallchel <sebastianalfabc@gmail.com>
2018-11-12 12:45:41 +0300

    remove lib

--- lib/helpers/matchMaxMediaWidth.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type EventResize = {
-    target: Window | null;
-};
-declare const _default: (maxMediaWidth: number) => (e: Event | EventResize) => boolean;
-export default _default;
